9e89045399b6e24f2e9fdec763fbbfbdbd8811a8
Yuki <119506189+YukiiCode@users.noreply.github.com>
2025-04-03 13:55:56 +0200

    Create Provisional.sql

---
+++ ReserTable/Bbdd/Provisional.sql
@@ -0,0 +1,19 @@
+CREATE TABLE employees();
+CREATE TABLE employees_timetable();
+CREATE TABLE employees_penalties();
+
+CREATE TABLE products();
+CREATE TABLE products_ingredients();
+
+
+CREATE TABLE clients();
+CREATE TABLE clients_history();
+CREATE TABLE discount_coupons();
+CREATE TABLE clients_invoices();
+
+CREATE TABLE logs();
+
+CREATE TABLE inventory();
+
+CREATE TABLE events();
+CREATE TABLE events_offers();
